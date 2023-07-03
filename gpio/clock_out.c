245b641b3952290a854484e8ddbab0c75e6d1979
Graeme Winter <graeme.winter@gmail.com>
2023-07-03 04:50:54 +0100

    Basic GPIO

--- gpio/clock_out.c
+++ gpio/clock_out.c
@@ -18,4 +18,22 @@
 #define GPIO_ALT4 3
 #define GPIO_ALT5 2
 
-static volatile uint32_t reg = NULL;
+static volatile uint32_t *reg = NULL;
+
+void setup(void) {
+  int fd = open("/dev/gpiomem", O_RDWR | O_SYNC);
+  reg = (uint32_t *)mmap(NULL, 0xB4, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  close(fd);
+}
+
+void set_mode(int gpio, int mode) {
+  int off = gpio / 10;
+  int shift = 3 * (gpio % 10);
+  reg[off] = (reg[off] & ~(0b111 << shift)) | (mode << shift);
+}
+
+int main() {
+  setup();
+  set_mode(4, GPIO_ALT0);
+  return 0;
+}
