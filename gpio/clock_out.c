9a6771c1509e1e13fcdd6ee5614cb8dccaf7447d
Graeme Winter <graeme.winter@gmail.com>
2023-08-19 15:06:37 +0100

    Unused imports

--- gpio/clock_out.c
+++ gpio/clock_out.c
@@ -1,10 +1,7 @@
 #include <fcntl.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <string.h>
 #include <sys/mman.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
 
 // GPIO modes - datasheet table 6.2
